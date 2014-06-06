1f66f6d0e630a86d9bfa86c0412f69afb7185c66
vagrant <vagrant@precise32.(none)>
2014-06-06 10:36:28 +0000

    リファクタ中。

--- 2411/answer.rb
+++ 2411/answer.rb
@@ -1,39 +1,28 @@
 $MAX_DAY = 3_652_425
 
-def bsearch arr, first, last, consum, value
-  si = first
-  ei = last
-  target = -1
-  last_value = 0
-
-  loop do
-    break if si > ei
-
-    target = (ei + si) / 2
-    last_value = (consum < target ? arr[consum] : arr[target]) + target
-    if last_value > value
-      ei = target - 1
-    elsif last_value < value
-      si = target + 1
-    else
-      break
-    end
-  end
-
-  if last_value < value or target == -1
-    return -1
-  end
-
-  target
-end
-
 def main
   n,q = gets.chomp.split(' ').map(&:to_i)
   days = Array.new($MAX_DAY + 1, 0)
-  consum = 0
+  consum = Array.new($MAX_DAY + 1, 0)
+  final_index = 0
+  no_consum_index = 0
+
   n.times do
     w,t,x = gets.chomp.split(' ').map(&:to_i)
-    recover_day = bsearch(days, 0, days.size - 1, consum, w)
+    recover_day = -1
+    if consum[final_index] + final_index >= w
+      recover_day = final_index
+    else
+      (final_index+1).upto($MAX_DAY) do |i|
+        consum[i] = days[i] + consum[i-1]
+        if consum[i] + i >= w
+          final_index = i
+          recover_day = i
+          break
+        end
+      end
+    end
+
     if recover_day == -1
       puts 'Many years later'
       next
@@ -41,41 +30,26 @@ def main
 
     puts recover_day
 
-    if consum < recover_day
-      consum.upto(recover_day) do |i|
-        days[i] = days[consum]
-      end
-      consum = recover_day
+    index = 0
+    x.times do |i|
+      index = recover_day + 1 + i
+      break if index > $MAX_DAY
+      days[index] += (i+1) ** t
     end
-
-    recover_value = 0
-    prev = 0
-    x.times do |j|
-      break if (recover_day + 1 + j) > $MAX_DAY
-      index = recover_day + 1 + j
-      recover_value = begin
-        case t
-        when 0; 1
-        when 1; j+1
-        when 2; (j+1) ** 2
-        end
-      end
-      if days[index] == 0
-        days[index] = recover_value + days[index-1]
-      else
-        days[index] += prev + recover_value
-      end
-      prev += recover_value
-      consum = index if index > consum
-    end
+    no_consum_index = [no_consum_index, (index < $MAX_DAY ? index : $MAX_DAY)].max
   end
+
+  (final_index+1).upto(no_consum_index) do |i|
+    consum[i] = days[i] + consum[i-1]
+  end
+  final_index = no_consum_index
 
   q.times do
     target = gets.chomp.to_i
-    if target <= consum
-      puts target + days[target]
+    if target <= final_index
+      puts target + consum[target]
     else
-      puts target + days[consum]
+      puts target + consum[final_index]
     end
   end
 end
